b0881d47de844768583d36b0584cbd3674210bce
rmtier <ramimtier@gmail.com>
2023-04-16 13:59:56 +0200

    Add project files.

---
+++ LoremIpsumGenerator.cpp
@@ -0,0 +1,41 @@
+﻿// LoremIpsum++.cpp : Defines the entry point for the application.
+//
+
+#include "LoremIpsumGenerator.hpp"
+#include <iostream>
+#include <numeric>
+#include <functional>
+#include <chrono>
+
+void measure_time(std::string prefix, std::function<void(int)> functionForMeasure, long rounds)
+{
+	auto startTime = std::chrono::high_resolution_clock::now();
+
+	for (long i = 0; i < rounds; ++i)
+	{
+		functionForMeasure(i % 4);
+	}
+
+	auto endTime = std::chrono::high_resolution_clock::now();
+	std::chrono::duration< double > fs = endTime - startTime;
+
+	std::cout << prefix << " function time (ms): "
+		<< std::chrono::duration_cast<std::chrono::milliseconds>(fs) << std::endl;
+
+}
+
+int main()
+{
+	std::cout << "3:" << lorem_ipsum::generate_lorem_ipsum_lines<3>() << std::endl << std::endl;
+
+	std::cout << "R3:" << lorem_ipsum::generate_lorem_ipsum_lines(3) << std::endl << std::endl;
+
+
+	measure_time("Runtime", [](int i) {
+		lorem_ipsum::generate_lorem_ipsum_lines(24);
+		}, 100000);
+
+	measure_time("Static", [](int) {
+		lorem_ipsum::generate_lorem_ipsum_lines<24>();
+		}, 100000);
+}
---
+++ LoremIpsumGenerator.hpp
@@ -0,0 +1,122 @@
+﻿// LoremIpsum++.hpp : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#include <exception>
+#include <string> 
+#include <array>
+
+namespace lorem_ipsum
+{
+	static constexpr auto c_lines = std::to_array({
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+		" Sed non risus.",
+		" Cras elementum ultrices diam.",
+		" Maecenas ligula massa, varius a, semper congue, euismod non, mi.",
+		" Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.",
+		"\n\nDuis semper.",
+		" Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.",
+		" Pellentesque congue.",
+		" Ut in risus volutpat libero pharetra tempor.",
+		" Cras vestibulum bibendum augue.",
+		" Praesent egestas leo in pede.",
+		" Praesent blandit odio eu enim.",
+		" Pellentesque sed dui ut augue blandit sodales.",
+		"\n\nVestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh.",
+		" Mauris ac mauris sed pede pellentesque fermentum.",
+		" Maecenas adipiscing ante non diam sodales hendrerit."
+		"\n\nUt velit mauris, egestas sed, gravida nec, ornare ut, mi.",
+		" Aenean ut orci vel massa suscipit pulvinar.",
+		" Nulla sollicitudin.",
+		" Fusce varius, ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula.",
+		" Pellentesque rhoncus nunc et augue.",
+		" Integer id felis.",
+		"\n\nCurabitur aliquet pellentesque diam.",
+		" Integer quis metus vitae elit lobortis egestas.",
+		" Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+		" Morbi vel erat non mauris convallis vehicula.",
+		" Nulla et sapien.",
+		" Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam.",
+		"\n\nMauris ullamcorper felis vitae erat.",
+		" Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna."
+		});
+
+	class LongLoremIpsumException : public std::exception {
+		unsigned int m_size;
+	public:
+		LongLoremIpsumException(unsigned int size)
+		{
+			m_size = size;
+		}
+
+		std::string what() {
+			return "Cant generate that long lorem ipsum, size: " + std::to_string(m_size) 
+				+ ". Max size is: " + std::to_string(c_lines.size());
+		}
+	};
+
+
+	std::string extend(const std::string& str, const std::string& str2) {
+		return std::move(str + str2);
+	}
+
+	std::string generate_lorem_ipsum_lines_internal(unsigned int count)
+	{
+		if (count > 0)
+		{
+			return extend(generate_lorem_ipsum_lines_internal(count - 1), c_lines[count - 1]);
+		}
+		return "";
+	}
+
+
+	std::string generate_lorem_ipsum_lines(unsigned int count)
+	{
+		if (count > c_lines.size())
+		{
+			throw LongLoremIpsumException(count);
+		}
+		return generate_lorem_ipsum_lines_internal(count);
+	}
+
+	template<int ... T>
+	struct LoremIpsumStructure
+	{
+		std::string get()
+		{
+			return "";
+		}
+	};
+
+	template<int T, int ... Rest>
+	struct LoremIpsumStructure<T, Rest ...> : public LoremIpsumStructure<Rest ...>
+	{
+		using BASE = LoremIpsumStructure<Rest ... >;
+
+		std::string get()
+		{
+			return std::move(std::string(c_lines[T]) + static_cast<BASE>(*this).get());
+		}
+	};
+
+	template <int N, int ... Next>
+	struct GenerateLoremIpsumStructure : public GenerateLoremIpsumStructure<N - 1, N - 1, Next...>
+	{ };
+
+	template <int ... Next>
+	struct GenerateLoremIpsumStructure<0, Next ... >
+	{
+		using type = LoremIpsumStructure<Next ... >;
+	};
+
+	template <int linesCount>
+	constexpr std::string generate_lorem_ipsum_lines()
+	{
+		static_assert(linesCount < c_lines.size(), "Cant generate that long Lorem Ipsum");
+		static_assert(linesCount > 0, "Cant generate that small Lorem Ipsum");
+
+		typename GenerateLoremIpsumStructure<linesCount>::type lorem_ipsum;
+
+		return lorem_ipsum.get();
+	}
+}
